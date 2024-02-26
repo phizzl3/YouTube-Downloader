b38b778530e34d0bb3d2d68d9bd32654f81f85e5
Brandon Phillips <phizzl3@gmail.com>
2024-02-26 10:35:00 -0600

    Add version to main file

--- youtube_downloader/main.py
+++ youtube_downloader/main.py
@@ -8,23 +8,27 @@ yt-dlp --merge-output-format mp4 -o "/Volumes/T7_SSD/outputfile.py" "https://www
 
 import subprocess
 from modules.settings import OUT_FOLDER
+from modules import display
+
+__version__ = "1.0.0"
 
 
 def get_url() -> str:
     print("\n Please paste the YouTube URL for the video below.")
     return input(": ")
 
+
 def get_filename() -> str:
     print("\n Please enter the name for the output file below. (.mp4 will be added)")
     return input(": ")
+
 
 def generate_file_output(filename: str) -> str:
     if not filename:
         filename = "YouTube Download"
     return f"{OUT_FOLDER}/{filename}.mp4"
 
+
 def run_command(url: str, file_output: str) -> None:
     command: str = f'yt-dlp --merge-output-format mp4 -o "{file_output}" "{url}"'
     subprocess.run(command, shell=True, check=False)
-    
-
