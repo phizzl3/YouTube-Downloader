76707090821082629dacfde3e021c311b4079ee8
Brandon Phillips <phizzl3@gmail.com>
2024-02-26 10:50:23 -0600

    Update main.py
    Adds the main function calls and docstring.

--- youtube_downloader/main.py
+++ youtube_downloader/main.py
@@ -1,9 +1,9 @@
 """
 This assumes you already have yt-dlp installed. 
 Use homebrew: "brew install yt-dlp"
-
-Sample command:
-yt-dlp --merge-output-format mp4 -o "/Volumes/T7_SSD/outputfile.py" "https://www.youtube.com/watch?v=PXMJ6F"
+* Gets the URL of the video
+* Gets your desired output filename
+* Downloads a .mp4 version of the file to the specified folder
 """
 
 import subprocess
@@ -15,12 +15,12 @@ __version__ = "1.0.0"
 
 def get_url() -> str:
     print("\n Please paste the YouTube URL for the video below.")
-    return input(": ")
+    return input(" URL: ")
 
 
 def get_filename() -> str:
     print("\n Please enter the name for the output file below. (.mp4 will be added)")
-    return input(": ")
+    return input(" Name: ")
 
 
 def generate_file_output(filename: str) -> str:
@@ -31,4 +31,15 @@ def generate_file_output(filename: str) -> str:
 
 def run_command(url: str, file_output: str) -> None:
     command: str = f'yt-dlp --merge-output-format mp4 -o "{file_output}" "{url}"'
+    print()  # Just to add some space
     subprocess.run(command, shell=True, check=False)
+
+
+if __name__ == "__main__":
+    display.ascii_art(__version__)
+    video_url = get_url()
+    filename = get_filename()
+    output = generate_file_output(filename)
+    run_command(url=video_url, file_output=output)
+    display.output_message(file_location=output)
+    
